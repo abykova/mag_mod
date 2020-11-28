5fd3cf8f921ab666ca1d17f3ce12ba6fdc25c891
abykova <alex.94.girl@gmail.com>
2020-11-28 17:26:21 +0600

    module

---
+++ First/Stikers/Controller/Stikers/Index.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace First\Stikers\Controller\Stikers;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+	public function __construct(
+		\Magento\Framework\App\Action\Context $context)
+	{
+		return parent::__construct($context);
+	}
+
+	public function execute()
+	{
+		//действие для создания стикера на товарах по акции
+
+
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # mag_mod
+
+модуль для magento 2 
+
+встроить в /app/code
+
+запустить команды для инициализации модуля
+
+`php bin/magento module:enable First_Stikers`
